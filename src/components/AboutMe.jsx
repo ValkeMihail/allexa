006884562912b88dfa72a263a45f41d8e1195ede
Mihail Nicoară <mihail.ncr@gmail.com>
2023-05-27 11:11:06 +0300

    bugfix ui

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,11 +1,10 @@
-import  React ,{ useState, useRef, useEffect } from 'react';
+import  React ,{ useState,  useEffect } from 'react';
 import '../App.css';
 import { doc, getDoc } from 'firebase/firestore';
 import { db } from '../firebase'; // assuming firebase is initialized and exported as db
 
 function AboutMe() {
-  const [inView, setInView] = useState(false);
-  const borderLayRef = useRef(null);
+
   const [myImage, setMyImage] = useState('');
   const [bio, setBio] = useState('');
 
@@ -19,43 +18,27 @@ function AboutMe() {
       }
     });
   
-    const options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.5,
-    };
-  
-    const observer = new IntersectionObserver(([entry]) => {
-      setInView(entry.isIntersecting);
-    }, options);
-  
-    const currentRef = borderLayRef.current;
-  
-    if (currentRef) {
-      observer.observe(currentRef);
-    }
-  
-    return () => {
-      if (currentRef) {
-        observer.unobserve(currentRef);
-      }
-    };
   }, []);
   
 
   return (
     <div className="about-container">
-      <img id="aboutmeImage" src={myImage} alt="" loading='lazy' />
-      <h3>Photographer</h3>
-      <h2 id='aboutmeheader'>Allexa</h2>
-      <div className='separator'>
-        <h1>About Me</h1>
-        <div className={inView ? 'borderLay animated3' : 'borderLay'} ref={borderLayRef}>
-          <p>
-            {bio}
-          </p>
+        <div className='headerAbout'>
+            <img id="aboutmeImage" src={myImage} alt="" loading='lazy' />
+            <div className='aboutTitle'>
+                <h3>Photographer</h3>
+                <h2 id='aboutmeheader'>Allexa</h2>
+            </div>
+            
         </div>
-      </div>
+        <div className='description'>
+            <h1>About Me</h1>
+            <div>
+                <p>
+                    {bio}
+                </p>
+            </div>
+        </div>
     </div>
   );
 }
